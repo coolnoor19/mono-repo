4bdb14edb231b468378635cbccc78f26d422abca
Noor Alam <nooralam192001@gmail.com>
2026-08-16 22:30:40 +0530

    feat : as we have created new class so for it we have to make object like we made previously, we make it in packages/trpc/sercer/services/index.ts

--- packages/trpc/server/services/index.ts
+++ packages/trpc/server/services/index.ts
@@ -1,3 +1,6 @@
 import UserService from "@repo/services/user" // importing the UserService class from the user service module
+import FormService from "@repo/services/form"
 
 export const userService = new UserService() // creating an instance of the UserService class and exporting it for use in other parts of the application
+
+export const formService = new FormService()

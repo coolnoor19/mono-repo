522eb0c8b6cb484e37fa806f445af8ca03e16988
Noor Alam <nooralam192001@gmail.com>
2026-08-17 01:08:19 +0530

    feat: implement FormField functionality with creation and retrieval of fields in the form builder

--- packages/trpc/server/services/index.ts
+++ packages/trpc/server/services/index.ts
@@ -1,6 +1,7 @@
 import UserService from "@repo/services/user" // importing the UserService class from the user service module
 import FormService from "@repo/services/form"
-
+import FormFieldService from "@repo/services/form-field"
 export const userService = new UserService() // creating an instance of the UserService class and exporting it for use in other parts of the application
 
 export const formService = new FormService()
+export const formFieldService = new FormFieldService()
